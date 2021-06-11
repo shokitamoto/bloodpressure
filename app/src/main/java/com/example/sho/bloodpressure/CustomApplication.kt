2f0bc8db21279a7e935d0e75fcb4a4cf40045af3
shokitamoto <kitamoto.ambitious@gmail.com>
2021-06-11 16:45:02 +0900

    6/11 bloodpressure CustomRecyclerViewAdapterクラスの27行目「format」でエラー

---
+++ app/src/main/java/com/example/sho/bloodpressure/CustomApplication.kt
@@ -0,0 +1,15 @@
+package com.example.sho.bloodpressure
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class CustomApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val config = RealmConfiguration.Builder().build()
+        Realm.setDefaultConfiguration(config)
+    }
+}
+
---
+++ app/src/main/java/com/example/sho/bloodpressure/CustomRecyclerViewAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.sho.bloodpressure
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.RealmResults
+import android.view.View
+import android.widget.TextView
+import java.text.DateFormat
+
+class CustomRecyclerViewAdapter(realmResults: RealmResults<BloodPress>) : RecyclerView.Adapter<ViewHolder>() {
+     private val rResults: RealmResults<BloodPress> = realmResults
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.one_result, parent, false)
+        val viewholder = ViewHolder(view)
+        return viewholder
+    }
+
+    override fun getItemCount(): Int {
+        return rResults.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val bloodPress = rResults[position]
+        holder.dateText?.text = DateFormat.format("yyyy/MM/dd kk:mm", bloodPress?.dateTime)
+        holder.minMaxText?.text = "${bloodPress?.max.toString()}/${bloodPress?.min.toString()}"
+        holder.pulseText?.text = bloodPress?.pulse.toString()
+        holder.itemView.setBackgroundColor(if (position % 2 == 0) Color.LTGRAY else Color.WHITE)
+    }
+}
